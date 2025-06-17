f01afc3d71abe62a9610b097553e005aa042ab56
Deep soni <fichadiyadeep@gmail.com>
2025-06-17 22:07:09 +0530

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,7 @@
-"use client";
+"use client"
+
+import { motion } from "framer-motion"
+import { PhoneCall } from "lucide-react"
 
 import { useState } from "react";
 import { Menu, X, Phone } from "lucide-react";
